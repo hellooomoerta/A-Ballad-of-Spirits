d1e59792c1b959e0e0c352c88f19124e60ba73a5
hellooomoerta <hilding_1989@hotmail.com>
2024-03-18 11:02:40 +0100

    Player Animation, Bush, Tree, Slime Sprite

--- A Ballad of Spirits/Assets/Scripts/PlayerController.cs
+++ A Ballad of Spirits/Assets/Scripts/PlayerController.cs
@@ -7,11 +7,15 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float moveSpeed = 1f;
 
     Vector2 movement;
+    Animator myAnimator;
+    SpriteRenderer mySpriteRenderer;
     Rigidbody2D rb;
     PlayerControls playerControls;
 
     private void Awake()
     {
+        myAnimator = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
         playerControls = new PlayerControls();
     }
@@ -24,16 +28,35 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         Move();
+        FlipSprite();
     }
 
     void PlayerInput()
     {
         movement = playerControls.Movement.Move.ReadValue<Vector2>().normalized;
+
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
     }
 
     void Move()
     {
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    void FlipSprite()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerPosition = Camera.main.WorldToScreenPoint(transform.position);
+
+        if (mousePos.x < playerPosition.x)
+        {
+            mySpriteRenderer.flipX = true;
+        }
+        else
+        {
+            mySpriteRenderer.flipX = false;
+        }
     }
 
     private void OnEnable()
