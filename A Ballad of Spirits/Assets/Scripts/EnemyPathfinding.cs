fe394a2217d6486d663ff483ed6f20f2bcf902e9
hellooomoerta <hilding_1989@hotmail.com>
2024-03-18 11:31:10 +0100

    Basic EnemyAI and Pathfinding

---
+++ A Ballad of Spirits/Assets/Scripts/EnemyPathfinding.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathfinding : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 2f;
+
+    Vector2 moveDirection;
+    Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void FixedUpdate()
+    {
+        Move();
+        Debug.Log(moveDirection);
+    }
+
+    void Move()
+    {
+        rb.MovePosition(rb.position + moveDirection * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    public void MoveTo(Vector2 targetPosition)
+    {
+        moveDirection = targetPosition;
+    }
+}
