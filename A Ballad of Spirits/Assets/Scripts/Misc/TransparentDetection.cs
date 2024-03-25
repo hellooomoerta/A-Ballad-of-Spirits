2168d2b5d17d5cc1bd534363658fa8e0c8741bfb
hellooomoerta <hilding_1989@hotmail.com>
2024-03-25 11:59:26 +0100

    Fade Script

--- A Ballad of Spirits/Assets/Scripts/Misc/TransparentDetection.cs
+++ A Ballad of Spirits/Assets/Scripts/Misc/TransparentDetection.cs
@@ -1,18 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class TransparentDetection : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Range(0, 1)]
+    [SerializeField] float transparencyAmount = .5f;
+    [SerializeField] float fadeTime = .2f;
+
+    SpriteRenderer spriteRenderer;
+    Tilemap tilemap;
+
+    private void Awake()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        tilemap = GetComponent<Tilemap>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.gameObject.GetComponent<PlayerController>())
+        {
+            if (spriteRenderer)
+            {
+                StartCoroutine(FadeRoutine(spriteRenderer, fadeTime, spriteRenderer.color.a, transparencyAmount));
+            }
+            else if (tilemap)
+            {
+                StartCoroutine(FadeRoutine(tilemap, fadeTime, tilemap.color.a, transparencyAmount));
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<PlayerController>())
+        {
+            if (spriteRenderer)
+            {
+                StartCoroutine(FadeRoutine(spriteRenderer, fadeTime, spriteRenderer.color.a, 1f));
+            }
+            else if (tilemap)
+            {
+                StartCoroutine(FadeRoutine(tilemap, fadeTime, tilemap.color.a, 1f));
+            }
+        }
+    }
+
+    IEnumerator FadeRoutine(SpriteRenderer spriteRenderer, float fadeTime, float startValue, float targetTransparency)
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < fadeTime)
+        {
+            elapsedTime += Time.deltaTime;
+            float newAlpha = Mathf.Lerp(startValue, targetTransparency, elapsedTime / fadeTime);
+            spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.b, spriteRenderer.color.g, newAlpha);
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeRoutine(Tilemap tilemap, float fadeTime, float startValue, float targetTransparency)
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < fadeTime)
+        {
+            elapsedTime += Time.deltaTime;
+            float newAlpha = Mathf.Lerp(startValue, targetTransparency, elapsedTime / fadeTime);
+            tilemap.color = new Color(tilemap.color.r, tilemap.color.b, tilemap.color.g, newAlpha);
+            yield return null;
+        }
     }
 }
