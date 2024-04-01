d482649fc21de6563325465ace886abd7988e820
hellooomoerta <hilding_1989@hotmail.com>
2024-04-01 09:32:20 +0200

    Problems...

--- A Ballad of Spirits/Assets/Scripts/Scene Management/Area Exit.cs
+++ A Ballad of Spirits/Assets/Scripts/Scene Management/Area Exit.cs
@@ -8,12 +8,26 @@ public class AreaExit : MonoBehaviour
     [SerializeField] string sceneToLoad;
     [SerializeField] string sceneTransitionName;
 
+    float waitToLoadTime = .5f;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.GetComponent<PlayerController>())
         {
-            SceneManager.LoadScene(sceneToLoad);
             SceneManagement.Instance.SetTransitionName(sceneTransitionName);
+            UIFade.Instance.FadeToBlack();
+            StartCoroutine(LoadSceneRoutine());
         }
+    }
+
+    IEnumerator LoadSceneRoutine()
+    {
+        while (waitToLoadTime >= 0)
+        {
+            waitToLoadTime -= Time.deltaTime;
+            yield return null;
+        }
+
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
