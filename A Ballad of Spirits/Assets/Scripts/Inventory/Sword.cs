1b9de7da35cd133ddaf361d9f5b26101ab515fb9
hellooomoerta <hilding_1989@hotmail.com>
2024-04-08 14:24:50 +0200

    Different Weapons part 1

--- A Ballad of Spirits/Assets/Scripts/Inventory/Sword.cs
+++ A Ballad of Spirits/Assets/Scripts/Inventory/Sword.cs
@@ -2,30 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sword : MonoBehaviour
+public class Sword : MonoBehaviour, IWeapon
 {
+    
+    
+    [SerializeField] float swordAttackCD = 0.2f;
     [SerializeField] GameObject slashEffectPrefab;
     [SerializeField] Transform SlashAnimSpawnPoint;
     [SerializeField] Transform swordCollider;
 
     GameObject slashAnim;
     Animator myAnimator;
-    PlayerControls playerControls;
     PlayerController playerController;
     ActiveWeapon activeWeapon;
 
     private void Awake()
     {
         myAnimator = GetComponent<Animator>();
-        playerControls = new PlayerControls();
         playerController = GetComponentInParent<PlayerController>();
         activeWeapon = GetComponentInParent<ActiveWeapon>();
         swordCollider.gameObject.SetActive(false);
-    }
-
-    void Start()
-    {
-        playerControls.Combat.Attack.started += _ => Attack();
     }
 
     private void Update()
@@ -33,13 +29,20 @@ public class Sword : MonoBehaviour
         MouseFollowWithOffset();
     }
 
-    void Attack()
+    public void Attack()
     {
+        //isAttacking = true;
         myAnimator.SetTrigger("Attack");
         swordCollider.gameObject.SetActive(true);
-
         slashAnim = Instantiate(slashEffectPrefab, SlashAnimSpawnPoint.position, Quaternion.identity);
         slashAnim.transform.parent = this.transform.parent;
+        StartCoroutine(AttackCDRoutine());
+    }
+
+    IEnumerator AttackCDRoutine()
+    {
+        yield return new WaitForSeconds(swordAttackCD);
+        ActiveWeapon.Instance.ToggleIsAttacking(false);
     }
 
     public void DoneAttackingAnimEvent()
@@ -82,10 +85,5 @@ public class Sword : MonoBehaviour
             activeWeapon.transform.rotation = Quaternion.Euler(0, 0, 0);
             swordCollider.transform.rotation = Quaternion.Euler(0, 0, 0);
         }
-    }
-
-    private void OnEnable()
-    {
-        playerControls.Enable();
     }
 }
--- A Ballad of Spirits/Assets/Scripts/Player/ActiveWeapon.cs
+++ A Ballad of Spirits/Assets/Scripts/Player/ActiveWeapon.cs
@@ -2,17 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ActiveWeapon : MonoBehaviour
+public class ActiveWeapon : Singleton<ActiveWeapon>
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] MonoBehaviour currentActiveWeapon;
+
+    bool attackButtonDown, isAttacking = false;
+
+    PlayerControls playerControls;
+
+
+    protected override void Awake()
     {
-        
+        base.Awake();
+        playerControls = new PlayerControls();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        playerControls.Combat.Attack.started += _ => StartAttacking();
+        playerControls.Combat.Attack.canceled += _ => StopAttacking();
+    }
+
+    private void Update()
+    {
+        Attack();
+    }
+
+    public void ToggleIsAttacking(bool value)
+    {
+        isAttacking = value;
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    void StartAttacking()
+    {
+        attackButtonDown = true;
+    }
+
+    void StopAttacking()
+    {
+        attackButtonDown = false;
+    }
+
+    void Attack()
+    {
+        if (attackButtonDown && !isAttacking)
+        {
+        isAttacking = true;
+        (currentActiveWeapon as IWeapon).Attack();
+        }
     }
 }
